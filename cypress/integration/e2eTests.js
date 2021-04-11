59e9c2e15846b9d874d9d32dac2338d0964f8efc
Henri Mattila <henri19102@student.hamk.fi>
2021-04-11 12:40:10 +0300

    e2e test done

--- cypress/integration/e2eTests.js
+++ cypress/integration/e2eTests.js
@@ -4,4 +4,9 @@ describe('Pokedex', function () {
     cy.contains('ivysaur'); // eslint-disable-line
     cy.contains("Pokémon and Pokémon character names are trademarks of Nintendo.");// eslint-disable-line
   })
+  it('Kakuna pokemon page can be opened', function () {
+    cy.visit('http://localhost:5000'); // eslint-disable-line
+    cy.contains('kakuna').click(); // eslint-disable-line
+    cy.contains("shed skin");// eslint-disable-line
+  })
 })
